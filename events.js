fe0eb350581433f016309a6cf1c68ae8e665e63c
LewisMor5 <morrow-l10@ulster.ac.uk>
2026-04-21 17:55:20 +0100

    deleting export

--- events.js
+++ events.js
@@ -140,7 +140,6 @@ if (btns.length > 0) {
 function save() {
     localStorage.setItem("events", JSON.stringify(events));
     events = JSON.parse(localStorage.getItem("events"));
-    export{events};
 }
 save();
 
