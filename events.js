38ede0e62ec20fb4ba5fdccdeab704aad57a2d3d
Nixon <207246838+MagnaMuliebris-UK@users.noreply.github.com>
2026-04-22 11:29:03 +0100

    Update event date format validation
    Convert event date format from DD/MM/YYYY to YYYY-MM-DD.

--- events.js
+++ events.js
@@ -182,6 +182,8 @@ function validateFormData(){
 
         //eventDate
         let eventDate = document.getElementById("eventDate").value;
+        let [days, months, years] = eventDate.split('/');
+        eventDate = `${years}-${months}-${days}`;
         let eDateRegex= /^(?:\d{4}-(?:(?:(?:(?:0[13578]|1[02])-(?:0[1-9]|[1-2][0-9]|3[01]))|(?:(?:0[469]|11)-(?:0[1-9]|[1-2][0-9]|30))|(?:02-(?:0[1-9]|1[0-9]|2[0-8]))))|(?:(?:\d{2}(?:0[48]|[2468][048]|[13579][26]))|(?:(?:[02468][048])|[13579][26])00)-02-29)$/; // YYYY-MM-DD format, also accounts for leap years, month variable days, etc. Tested via regex101
         let eDateValid = RegExp(eDateRegex).test(eventDate);
 
